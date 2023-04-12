4dfdfa6c6e5304b86a1586ae7dd58a4d636cb8fc
Khaled Pendleton <hi@khaledpendleton.com>
2023-04-12 14:39:43 -0400

    Enables Postgis for geospatial querying

--- apps/missed_u/mix.exs
+++ apps/missed_u/mix.exs
@@ -42,7 +42,9 @@ defmodule MissedU.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.2"},
       {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"}
+      {:finch, "~> 0.13"},
+      {:geo, "~> 3.4"},
+      {:geo_postgis, "~> 3.4"}
     ]
   end
 
--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,8 @@ config :missed_u, MissedU.Repo,
   database: "missed_u_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  types: MissedU.PostgresTypes
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
